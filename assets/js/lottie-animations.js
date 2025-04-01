73a246bcf5bf65edab966a1208930a17c570965d
mastertroy0827 <mastertroy0827@gmail.com>
2025-04-01 21:29:36 +0900

    fix-lottie-animation-random

--- assets/js/lottie-animations.js
+++ assets/js/lottie-animations.js
@@ -19,13 +19,15 @@ function getRandomLottieFiles(num) {
   return shuffled.slice(0, num);
 }
 
+const selectedFiles1 = getRandomLottieFiles(24);
+const selectedFiles2 = getRandomLottieFiles(24);
+const selectedFiles3 = getRandomLottieFiles(24);
+
 // Render the animations in a grid
 function renderLottieGrid() {
   const gridContainer = document.getElementById("mainvisual_shapes_top");
-  gridContainer.innerHTML = ""; // Clear previous animations
-  const selectedFiles = getRandomLottieFiles(24);
 
-  selectedFiles.forEach((url, index) => {
+  selectedFiles1.forEach((url, index) => {
     if (itemSize > 64) {
       if (index > 21) {
         const player = document.createElement("dotlottie-player");
@@ -64,10 +66,8 @@ function renderLottieGrid() {
 
 function renderLottieGridBottom1() {
   const gridContainer = document.getElementById("mainvisual_shapes_bottom1");
-  gridContainer.innerHTML = ""; // Clear previous animations
-  const selectedFiles = getRandomLottieFiles(24);
 
-  selectedFiles.forEach((url, index) => {
+  selectedFiles2.forEach((url, index) => {
     const player = document.createElement("dotlottie-player");
     player.src = url;
     player.loop = true;
@@ -84,9 +84,8 @@ function renderLottieGridBottom1() {
 function renderLottieGridBottom2() {
   const gridContainer = document.getElementById("mainvisual_shapes_bottom2");
   gridContainer.innerHTML = ""; // Clear previous animations
-  const selectedFiles = getRandomLottieFiles(24);
 
-  selectedFiles.forEach((url, index) => {
+  selectedFiles3.forEach((url, index) => {
     const player = document.createElement("dotlottie-player");
     player.src = url;
     player.loop = true;
@@ -100,14 +99,53 @@ function renderLottieGridBottom2() {
   });
 }
 
+function renderLottieGridDraw() {  
+  const gridContainer = document.getElementById("mainvisual_shapes_top");
+  const dotlottieItems = gridContainer.getElementsByTagName("dotlottie-player");
+  for(let i = 0; i < dotlottieItems.length; i++) {
+    i % Math.floor(Math.random() * 10) == 0
+        ? (dotlottieItems[i].style.opacity = 100)
+        : (dotlottieItems[i].style.opacity = 0);
+    if(i > 21) {dotlottieItems[i].style.opacity = 0;}
+    if ((i + 1) % 8 == 0) {dotlottieItems[i].style.opacity = 0;}
+  };
+}
+
+function renderLottieGridBottomDraw1() {
+  const gridContainer = document.getElementById("mainvisual_shapes_bottom1");
+  const dotlottieItems = gridContainer.getElementsByTagName("dotlottie-player");
+  console.log(dotlottieItems);
+  
+  for(let i = 0; i < dotlottieItems.length; i++) {
+    i % Math.floor(Math.random() * 10) == 0
+        ? (dotlottieItems[i].style.opacity = 100)
+        : (dotlottieItems[i].style.opacity = 0);
+  };
+}
+
+function renderLottieGridBottomDraw2() {
+  const gridContainer = document.getElementById("mainvisual_shapes_bottom2");
+  const dotlottieItems = gridContainer.getElementsByTagName("dotlottie-player");
+  for(let i = 0; i < dotlottieItems.length; i++) {
+    i % Math.floor(Math.random() * 10) == 0
+        ? (dotlottieItems[i].style.opacity = 100)
+        : (dotlottieItems[i].style.opacity = 0);
+  };
+}
+
 renderLottieGrid();
 renderLottieGridBottom1();
 renderLottieGridBottom2();
+
 setInterval(() => {
-  renderLottieGrid();
-  renderLottieGridBottom1();
-  renderLottieGridBottom2();
-}, 3000);
+  renderLottieGridBottomDraw1();
+  setTimeout(() => {
+    renderLottieGridBottomDraw2();
+    setTimeout(() => {
+      renderLottieGridDraw();
+    }, 2000);
+  }, 2000);
+}, 6000);
 
 // ---------------------------
 
@@ -122,7 +160,9 @@ function getRandomLottie() {
 
 function createLottieGrid(k) {
   let container = document.getElementById("mainvisual_shapes_center-top");
-  if(k){container = document.getElementById("mainvisual_shapes_center-bottom");}
+  if (k) {
+    container = document.getElementById("mainvisual_shapes_center-bottom");
+  }
   for (let i = 0; i < 20; i++) {
     const player = document.createElement("dotlottie-player");
     player.src = getRandomLottie();
@@ -139,7 +179,9 @@ const lottieContainer = document.getElementById("mainvisual_shapes_center-top");
 const lottieClone = lottieContainer.cloneNode(true);
 lottieClone.style.position = "absolute";
 lottieClone.style.left = `${lottieContainer.offsetWidth}px`; // Place it right after the first set
-document.getElementById("mainvisual_shapes_center-top").appendChild(lottieClone);
+document
+  .getElementById("mainvisual_shapes_center-top")
+  .appendChild(lottieClone);
 
 let posX = 0;
 let speed = 2;
@@ -157,15 +199,19 @@ setInterval(() => {
 
 slideAnimation();
 
-const lottieContainerDown = document.getElementById("mainvisual_shapes_center-bottom");
+const lottieContainerDown = document.getElementById(
+  "mainvisual_shapes_center-bottom"
+);
 const lottieCloneDown = lottieContainerDown.cloneNode(true);
 lottieCloneDown.style.position = "absolute";
 lottieCloneDown.style.left = `${lottieContainerDown.offsetWidth}px`; // Place it right after the first set
-document.getElementById("mainvisual_shapes_center-bottom").appendChild(lottieCloneDown);
+document
+  .getElementById("mainvisual_shapes_center-bottom")
+  .appendChild(lottieCloneDown);
 let posXDown = 0;
-function slideAnimationDown () {  
+function slideAnimationDown() {
   posXDown -= speed;
-  if(posXDown <= -300) {
+  if (posXDown <= -300) {
     posXDown = 0;
   }
   lottieContainerDown.style.transform = `translateX(${posXDown}px)`;
